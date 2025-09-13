b185ab0687d9ce6ef3afdd7b84b19fde2756f80b
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2025-09-13 15:23:32 +0200

    changed os for Path (#4)

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,25 +1,23 @@
 from pathlib import Path
-import os
 
-import pandas
 import yaml
 
 from src.custom_exceptions import CustomException
 from src.logger import get_logger
-
 
 logger = get_logger(__name__)
 
 
 def read_yaml(file_path):
     try:
-        if not os.path.exist(file_path):
+        if not Path.exists(file_path):
             raise FileNotFoundError(f"file {file_path} not found")
 
-        with open(file_path, "r") as yml_file:
+        with Path.open(file_path, "r") as yml_file:
             config = yaml.safe_load(yml_file)
             logger.info("YAML file successfully read")
             return config
 
     except Exception as e:
         logger.error("Error while reading YAML file")
+        raise CustomException("Failed to read YAML file") from e
