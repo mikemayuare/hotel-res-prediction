1897f62b6a9393c31d739b6309cc555961c6181a
Miguelangel Leon <migueleonm@gmail.com>
2025-09-14 19:06:36 +0200

    Changed saving format to parquet to improve speed and efficiency

--- config/paths_config.py
+++ config/paths_config.py
@@ -3,13 +3,13 @@ from pathlib import Path
 # Data ingestion
 RAW_DIR = Path("artifacts/raw")
 RAW_FILE_PATH = Path(f"{RAW_DIR}/raw.csv")
-TRAIN_FILE_PATH = Path(f"{RAW_DIR}/train.csv")
-TEST_FILE_PATH = Path(f"{RAW_DIR}/test.csv")
+TRAIN_FILE_PATH = Path(f"{RAW_DIR}/train.parquet")
+TEST_FILE_PATH = Path(f"{RAW_DIR}/test.parquet")
 
 # config
 CONFIG_PATH = Path("config/config.yaml")
 
 # data processing
 PROCESSED_DIR = "artifacts/processed"
-PROCESSED_TRAIN_DATA_PATH = Path(f"{PROCESSED_DIR}/processed_train.csv")
-PROCESSED_TEST_DATA_PATH = Path(f"{PROCESSED_DIR}/processed_test.csv")
+PROCESSED_TRAIN_DATA_PATH = Path(f"{PROCESSED_DIR}/processed_train.parquet")
+PROCESSED_TEST_DATA_PATH = Path(f"{PROCESSED_DIR}/processed_test.parquet")
