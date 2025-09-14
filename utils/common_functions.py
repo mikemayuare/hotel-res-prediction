2673c7071276695b169bf4da781c499239e11650
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2025-09-14 14:04:02 +0200

    Feat add csv loader (#8)
    * added artifacts folder content
    
    * Added csv to pandas funtion

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pathlib import Path
 
 import yaml
@@ -8,7 +9,19 @@ from src.logger import get_logger
 logger = get_logger(__name__)
 
 
-def read_yaml(file_path):
+def read_yaml(file_path: Path) -> None:
+    """Reads a YAML file
+
+    Args:
+        file_path: Path, path of the YAML file.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: in case file_path is not found
+        CustomException: In case YAML is not readable
+    """
     try:
         if not Path(file_path).exists():
             raise FileNotFoundError(f"file {file_path} not found")
@@ -19,5 +32,25 @@ def read_yaml(file_path):
             return config
 
     except Exception as e:
-        logger.error("Error while reading YAML file")
+        logger.error("%s - Error while reading YAML file", str(e))
         raise CustomException("Failed to read YAML file") from e
+
+
+def load_data(file_path: Path) -> pd.DataFrame:
+    """Transforms CSV into a pandas DataFrame
+
+    Args:
+        file_path: Path, path of the CSV file
+
+    Returns:
+        pd.DataFrame: DataFrame with the CSV file content
+
+    Raises:
+        CustomException: In case pandas fails to read the file.
+    """
+    try:
+        logger.info("Loading data")
+        return pd.read_csv(file_path)
+    except Exception as e:
+        logger.error("%s - Error loading the data", str(e))
+        raise CustomException("Failed to load data") from e
