f32d091c02dcea60c588e01a6bb8d371be8de297
Miguelangel Leon <migueleonm@gmail.com>
2025-09-28 11:08:58 +0200

    added parameters file to tune

---
+++ config/model_params.py
@@ -0,0 +1,16 @@
+# Defines the hyperparameter search space for LightGBM (low-level API).
+def lgbm_search_space(trial):
+    return {
+        "objective": "binary",
+        "metric": "binary_logloss",
+        "verbosity": -1,
+        "boosting_type": "gbdt",
+        "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
+        "num_leaves": trial.suggest_int("num_leaves", 2, 256),
+        "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
+        "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+        "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
+        "random_state": 42,
+    }
